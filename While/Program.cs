119f97b14058d5bdcd842a4aae5cc81d8b966c19
Aron <aronmn@vgy.se>
2016-09-23 14:48:49 +0200

    Övning 3

--- While/Program.cs
+++ While/Program.cs
@@ -9,7 +9,28 @@ namespace While
     class Program
     {
         static void Main(string[] args)
-        {
+        { //Övning 1
+           
+            int i = 0;
+            while (i < 100)
+            {
+                i++;
+                Console.WriteLine(i);
+            }
+            //Övning 2
+            Console.WriteLine("Mata in ditt lösenord");
+            var password = Console.ReadLine();
+           
+                while (password != "Aron")
+            {
+                Console.WriteLine("Mata in igen");
+                password = Console.ReadLine();
+            }
+               //Övning 3
+
+         
+            
+            
         }
     }
 }
